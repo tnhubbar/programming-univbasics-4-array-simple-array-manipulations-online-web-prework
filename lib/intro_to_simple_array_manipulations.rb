4ef882934251bab8de576f933997ed41aeee5aa2
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-06 02:05:08 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,32 +1,48 @@
-colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-    next_color = "violet"
-  colors_in_the_rainbow.push(next_color)
+
+def using_push(array, string)
+  array.push(string)
 end
 
-def  using_unshift
-  bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
-  bouroughs_in_nyc.unshift "Staten Island"
-  
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def using_pop
-  continents.pop 
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args
-  dog_breeds.pop(2)
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def using_shift 
-  my_favorite_cities.shift 
+def using_shift(array)
+  array.shift
 end
 
-def shift_with_args
-  ice_cream_brands.shift(2)
+def shift_with_args(array)
+  array.shift(2)
 end
 
-my_favorite_thing.concat(more_favs)
-list_of_programming_languages.insert(another_language)
-new_array=haircuts.uniq 
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
 
-  
+def using_insert(array, item)
+  array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
